f49e12533c95c74d7b72841bfe5d57b24797245c
HakerStone <102145491+HakerStone@users.noreply.github.com>
2023-11-01 23:00:17 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.getElementById('card-template');
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function createCard(cardData, deleteCardFunction) {
+  const card = cardTemplate.content.cloneNode(true);
+
+  card.querySelector('.card__image').src = cardData.link;
+  card.querySelector('.card__image').alt = cardData.name;
+  card.querySelector('.card__title').textContent = cardData.name;
+  card.querySelector('.card__delete-button').addEventListener('click', deleteCardFunction);
+
+  return card;
+}
+
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+    const card = event.target.closest('.card');
+    card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function renderCards() {
+  initialCards.forEach(card => {
+    placesList.append(createCard(card, deleteCard));
+  });
+}
+document.addEventListener('DOMContentLoaded', function() {
+    renderCards();
+});
