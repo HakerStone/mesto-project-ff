24bce681b68b20d17e146b165d23781be6ad46c0
HakerStone <102145491+HakerStone@users.noreply.github.com>
2024-01-22 17:20:07 +0300

    Исправления, добавление валидации и API

---
+++ src/scripts/components/api.js
@@ -0,0 +1,7 @@
+const config = {
+    baseUrl: 'https://nomoreparties.co/v4/wff-cohort-4',
+    headers: {
+      authorization: '96206b9a-7ad9-4df1-8447-204c90b6dab9',
+      'Content-Type': 'application/json'
+    }
+  }
